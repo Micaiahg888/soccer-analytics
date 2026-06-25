425b7fcf64846a6fd31a63d286fb67813364face
micaiah garan <lc23-0320@lclark.edu>
2026-06-24 22:26:08 -1000

    rearanged home screen and changed edit page

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-
 from flask import Flask, render_template, request, redirect
 
 from database import get_db
 
+
 app = Flask(__name__)
+
 
 
 
@@ -12,422 +13,692 @@ def home():
 
     conn = get_db()
 
-    # Get Players + their team names
-    players = conn.execute(
-        """
-        SELECT players.*, teams.name AS team_name
-        FROM players
-        JOIN teams
-        ON players.team_id = teams.id
-        """
-    ).fetchall()
 
 
-    # Get Teams
+    players = conn.execute("""
+
+    SELECT
+
+    players.id,
+
+    players.name,
+
+    teams.name AS team_name,
+
+
+    COALESCE(SUM(player_stats.goals),0)
+    AS goals,
+
+
+    COALESCE(SUM(player_stats.assists),0)
+    AS assists
+
+
+    FROM players
+
+
+    JOIN teams
+
+    ON players.team_id = teams.id
+
+
+    LEFT JOIN player_stats
+
+    ON players.id = player_stats.player_id
+
+
+    GROUP BY players.id
+
+
+    """).fetchall()
+
+
+
     teams = conn.execute(
         "SELECT * FROM teams"
     ).fetchall()
 
 
-    # Get Matches from database
-    matches = conn.execute(
-        """
-        SELECT matches.*, teams.name AS team_name
-        FROM matches
-        JOIN teams
-        ON matches.team_id = teams.id
-        """
-    ).fetchall()
+
+    matches = conn.execute("""
+
+    SELECT
+
+    matches.*,
+
+    teams.name AS team_name
 
 
-    # Match statistics
-    total_matches = len(matches)
-
-    total_goals = sum(
-        match["team_score"] for match in matches
-    )
+    FROM matches
 
 
-    wins = 0
-    losses = 0
-    draws = 0
+    JOIN teams
+
+    ON matches.team_id = teams.id
 
 
-    for match in matches:
+    """).fetchall()
 
-        if match["team_score"] > match["opponent_score"]:
-            wins += 1
-
-        elif match["team_score"] < match["opponent_score"]:
-            losses += 1
-
-        else:
-            draws += 1
-
-
-    goal_differential = sum(
-        match["team_score"] - match["opponent_score"]
-        for match in matches
-    )
-
-
-    if total_matches > 0:
-        win_percentage = (wins / total_matches) * 100
-    else:
-        win_percentage = 0
-
-
-
-    # Top scorers
-    top_scorers = conn.execute(
-        """
-        SELECT *
-        FROM players
-        ORDER BY goals DESC
-        LIMIT 5
-        """
-    ).fetchall()
-
-
-    # Top assisters
-    top_assisters = conn.execute(
-        """
-        SELECT *
-        FROM players
-        ORDER BY assists DESC
-        LIMIT 5
-        """
-    ).fetchall()
 
 
     conn.close()
 
 
+
     return render_template(
+
         "index.html",
+
         players=players,
+
         teams=teams,
-        matches=matches,
-        total_matches=total_matches,
-        total_goals=total_goals,
-		wins=wins,
-		losses=losses,
-		draws=draws,
-        goal_differential=goal_differential,
-        win_percentage=win_percentage,
-        top_scorers=top_scorers,
-        top_assisters=top_assisters
+
+        matches=matches
+
     )
 
-@app.route("/team/<int:team_id>")
-def team_page(team_id):
-
-    conn = get_db()
-
-
-    # Get team
-    team = conn.execute(
-        """
-        SELECT *
-        FROM teams
-        WHERE id = ?
-        """,
-        (team_id,)
-    ).fetchone()
 
 
 
-    # Get players
-    players = conn.execute(
-        """
-        SELECT *
-        FROM players
-        WHERE team_id = ?
-        """,
-        (team_id,)
-    ).fetchall()
+
+@app.route("/add_team", methods=["POST"])
+def add_team():
+
+    conn=get_db()
 
 
+    conn.execute(
 
-    # Get team matches
-    matches = conn.execute(
-        """
-        SELECT *
-        FROM matches
-        WHERE team_id = ?
-        """,
-        (team_id,)
-    ).fetchall()
+    "INSERT INTO teams(name) VALUES(?)",
+
+    (request.form["name"],)
+
+    )
 
 
-
-    # Stats
-    wins = 0
-    losses = 0
-    draws = 0
-    goals = 0
-
-
-    for match in matches:
-
-        goals += match["team_score"]
-
-
-        if match["team_score"] > match["opponent_score"]:
-            wins += 1
-
-        elif match["team_score"] < match["opponent_score"]:
-            losses += 1
-
-        else:
-            draws += 1
-
-
-
-    total_matches = len(matches)
-
-
-    if total_matches > 0:
-        win_percentage = (wins / total_matches) * 100
-    else:
-        win_percentage = 0
-
-
+    conn.commit()
 
     conn.close()
 
 
+    return redirect("/")
 
-    return render_template(
-        "team.html",
-        team=team,
-        players=players,
-        matches=matches,
-        wins=wins,
-        losses=losses,
-        draws=draws,
-        goals=goals,
-        win_percentage=win_percentage
-    )
+
+
+
 
 @app.route("/add_player", methods=["POST"])
 def add_player():
 
-	name = request.form["name"]
-	goals = int(request.form["goals"])
-	assists = int(request.form["assists"])
-	team_id = request.form["team_id"]
-
-	conn = get_db()
-
-	conn.execute(
-		"""
-		INSERT INTO players (team_id, name, goals, assists)
-		VALUES (?, ?, ?, ?)
-		""",
-		(	
-			team_id,
-			name,
-			goals,
-			assists
-		)
-	)
-	conn.commit()
-	conn.close()
-	
-	return redirect("/")
-
-@app.route("/delete/<int:player_id>")
-def delete_player(player_id):
-	
-	conn = get_db()
-	
-	conn.execute(
-		"DELETE FROM players WHERE id = ?",
-		(player_id,)
-	)
-
-	conn.commit()
-	conn.close()
-
-	return redirect("/")
-
-@app.route("/edit/<int:player_id>", methods=["GET", "POST"])
-def edit_player(player_id):
-	
-	conn = get_db()
-	
-	if request.method == "POST":
-
-		name = request.form["name"]
-		team_id = request.form["team_id"]
-		goals = request.form["goals"]
-		assists = request.form["assists"]
-		
-	
-		conn.execute(
-			"""
-			UPDATE players
-			SET name = ?, team_id = ?, goals = ?, assists = ?
-			WHERE id = ?
-			""",
-			(name, team_id, goals, assists, player_id)
-		)
-		
-		conn.commit()
-		conn.close()
-		
-		return redirect("/")
-
-	player = conn.execute(
-		"SELECT * FROM players WHERE id = ?",
-		(player_id,)
-	).fetchone()
-
-	teams = conn.execute(
-		"SELECT * FROM teams"
-	).fetchall()
-
-	conn.close()
-	
-	
+    conn=get_db()
 
 
-	return render_template("edit.html", player=player, teams=teams)
+    conn.execute(
 
-@app.route("/update/<int:player_id>", methods=["POST"])
-def update_player(player_id):
-	name = request.form["name"]
-	goals = request.form["goals"]
-	assists = request.form["assists"]
+    """
 
-	conn = get_db()
+    INSERT INTO players(team_id,name)
 
-	conn.execute(
-		"""
-		UPDATE players
-		SET name = ?, goals = ?, assists = ?
-		WHERE id = ?
-		""", 
-		(name, goals, assists, player_id)
-	)
+    VALUES(?,?)
 
-	conn.commit()
-	conn.close()
+    """,
+
+    (
+
+    request.form["team_id"],
+
+    request.form["name"]
+
+    )
+
+    )
 
 
-	return redirect("/")
+    conn.commit()
+
+    conn.close()
+
+
+    return redirect("/")
+
+
+
+
 
 @app.route("/add_match", methods=["POST"])
 def add_match():
 
-	team_id = request.form.get("team_id")
-	opponent = request.form.get("opponent")
-	date = request.form.get("date")
-	location = request.form.get("location")
-	season = request.form.get("season")
-	team_score = request.form.get("team_score")
-	opponent_score = request.form.get("opponent_score")
+    conn=get_db()
 
-	conn = get_db()
 
-	conn.execute(
-        """
-        INSERT INTO matches
-        (team_id, opponent, date, location, season, team_score, opponent_score)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-        """,
-        (
-            team_id,
-            opponent,
-            date,
-			location,
-			season,
-            team_score,
-            opponent_score
-        )
+    conn.execute(
+
+    """
+
+    INSERT INTO matches
+
+    (
+
+    team_id,
+
+    opponent,
+
+    date,
+
+    season,
+
+    location,
+
+    team_score,
+
+    opponent_score
+
     )
 
-	conn.commit()
-	conn.close()
 
-	return redirect("/")
+    VALUES(?,?,?,?,?,?,?)
 
-@app.route("/add_team", methods=["POST"])
-def add_team():
-	
-	name = request.form["name"]
-	conn = get_db()
 
-	conn.execute(
-		"INSERT INTO teams (name) VALUES (?)",
-		(name,)
-	)
+    """,
 
-	conn.commit()
-	conn.close()
+    (
 
-	return redirect("/")
+    request.form["team_id"],
 
-@app.route("/add_stat")
-def add_stat_page():
+    request.form["opponent"],
 
-    conn = get_db()
+    request.form["date"],
 
-    players = conn.execute("SELECT * FROM players").fetchall()
-    matches = conn.execute("SELECT * FROM matches").fetchall()
+    request.form["season"],
 
-    conn.close()
+    request.form["location"],
 
-    return render_template(
-        "add_stat.html",
-        players=players,
-        matches=matches
+    request.form["team_score"],
+
+    request.form["opponent_score"]
+
     )
 
-@app.route("/add_stat", methods=["POST"])
-def add_stat():
+    )
 
-    player_id = request.form["player_id"]
-    match_id = request.form["match_id"]
-    goals = request.form["goals"]
-    assists = request.form["assists"]
-    minutes = request.form["minutes"]
-
-    conn = get_db()
-
-    conn.execute("""
-        INSERT INTO player_stats
-        (player_id, match_id, goals, assists, minutes)
-        VALUES (?, ?, ?, ?, ?)
-    """, (player_id, match_id, goals, assists, minutes))
 
     conn.commit()
+
     conn.close()
 
+
     return redirect("/")
+
+
+
+
+
+@app.route("/team/<int:team_id>")
+def team_page(team_id):
+
+    conn=get_db()
+
+
+    team=conn.execute(
+
+    "SELECT * FROM teams WHERE id=?",
+
+    (team_id,)
+
+    ).fetchone()
+
+
+
+    players=conn.execute(
+
+    """
+
+    SELECT *
+
+    FROM players
+
+    WHERE team_id=?
+
+    """,
+
+    (team_id,)
+
+    ).fetchall()
+
+
+    conn.close()
+
+
+
+    return render_template(
+
+    "team.html",
+
+    team=team,
+
+    players=players
+
+    )
+
+
+
+
+
 
 @app.route("/player/<int:player_id>")
 def player_page(player_id):
 
-    conn = get_db()
+    conn=get_db()
 
-    player = conn.execute(
-        "SELECT * FROM players WHERE id = ?",
-        (player_id,)
+
+
+    player=conn.execute(
+
+    """
+
+    SELECT
+
+    players.*,
+
+    teams.name AS team_name
+
+
+    FROM players
+
+
+    JOIN teams
+
+    ON players.team_id=teams.id
+
+
+    WHERE players.id=?
+
+
+    """,
+
+    (player_id,)
+
     ).fetchone()
 
-    stats = conn.execute("""
-        SELECT player_stats.*, matches.opponent, matches.date
-        FROM player_stats
-        JOIN matches ON player_stats.match_id = matches.id
-        WHERE player_stats.player_id = ?
-    """, (player_id,)).fetchall()
+
+
+
+    stats=conn.execute(
+
+    """
+
+    SELECT
+
+
+    player_stats.*,
+
+
+    matches.opponent,
+
+    matches.date,
+
+    matches.season,
+
+    matches.location
+
+
+
+    FROM player_stats
+
+
+
+    JOIN matches
+
+
+    ON player_stats.match_id=matches.id
+
+
+
+    WHERE player_stats.player_id=?
+
+
+    """,
+
+    (player_id,)
+
+    ).fetchall()
+
+
+
+
+    totals=conn.execute(
+
+    """
+
+    SELECT
+
+
+    COUNT(*) AS games,
+
+
+    SUM(goals) AS goals,
+
+
+    SUM(assists) AS assists,
+
+
+    SUM(minutes) AS minutes
+
+
+
+    FROM player_stats
+
+
+    WHERE player_id=?
+
+
+    """,
+
+    (player_id,)
+
+    ).fetchone()
+
+
 
     conn.close()
 
-    return render_template("player.html", player=player, stats=stats)
 
 
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000, debug=True)
+    return render_template(
+
+    "player.html",
+
+    player=player,
+
+    stats=stats,
+
+    totals=totals
+
+    )
+
+
+
+
+
+
+@app.route("/add_stat", methods=["GET","POST"])
+def add_stat():
+
+    conn=get_db()
+
+
+
+    if request.method=="POST":
+
+
+        conn.execute(
+
+        """
+
+        INSERT INTO player_stats
+
+        (
+
+        player_id,
+
+        match_id,
+
+        goals,
+
+        assists,
+
+        minutes
+
+        )
+
+
+        VALUES(?,?,?,?,?)
+
+
+        """,
+
+        (
+
+        request.form["player_id"],
+
+        request.form["match_id"],
+
+        request.form["goals"],
+
+        request.form["assists"],
+
+        request.form["minutes"]
+
+        )
+
+        )
+
+
+        conn.commit()
+
+        conn.close()
+
+
+        return redirect("/")
+
+
+
+
+    players=conn.execute(
+        "SELECT * FROM players"
+    ).fetchall()
+
+
+
+    matches=conn.execute(
+        "SELECT * FROM matches"
+    ).fetchall()
+
+
+
+    conn.close()
+
+
+
+    return render_template(
+
+    "add_stat.html",
+
+    players=players,
+
+    matches=matches
+
+    )
+
+
+
+
+
+
+
+@app.route("/edit/<int:player_id>", methods=["GET","POST"])
+def edit_player(player_id):
+
+    conn=get_db()
+
+
+    if request.method=="POST":
+
+
+        conn.execute(
+
+        """
+
+        UPDATE players
+
+
+        SET name=?, team_id=?
+
+
+        WHERE id=?
+
+
+        """,
+
+        (
+
+        request.form["name"],
+
+        request.form["team_id"],
+
+        player_id
+
+        )
+
+        )
+
+
+        conn.commit()
+
+        conn.close()
+
+
+        return redirect(f"/player/{player_id}")
+
+
+
+    player=conn.execute(
+
+    "SELECT * FROM players WHERE id=?",
+
+    (player_id,)
+
+    ).fetchone()
+
+
+
+    teams=conn.execute(
+        "SELECT * FROM teams"
+    ).fetchall()
+
+
+    conn.close()
+
+
+    return render_template(
+
+    "edit_player.html",
+
+    player=player,
+
+    teams=teams
+
+    )
+
+
+
+
+
+@app.route("/edit_stat/<int:stat_id>", methods=["GET","POST"])
+def edit_stat(stat_id):
+
+    conn=get_db()
+
+
+    if request.method=="POST":
+
+
+        conn.execute(
+
+        """
+
+        UPDATE player_stats
+
+
+        SET goals=?,
+
+        assists=?,
+
+        minutes=?
+
+
+        WHERE id=?
+
+
+        """,
+
+        (
+
+        request.form["goals"],
+
+        request.form["assists"],
+
+        request.form["minutes"],
+
+        stat_id
+
+        )
+
+        )
+
+
+        conn.commit()
+
+        conn.close()
+
+
+        return redirect("/")
+
+
+
+
+    stat=conn.execute(
+
+    "SELECT * FROM player_stats WHERE id=?",
+
+    (stat_id,)
+
+    ).fetchone()
+
+
+
+    conn.close()
+
+
+    return render_template(
+
+    "edit_stat.html",
+
+    stat=stat
+
+    )
+
+
+
+
+
+@app.route("/delete_stat/<int:stat_id>")
+def delete_stat(stat_id):
+
+    conn=get_db()
+
+
+    conn.execute(
+
+    "DELETE FROM player_stats WHERE id=?",
+
+    (stat_id,)
+
+    )
+
+
+    conn.commit()
+
+    conn.close()
+
+
+    return redirect("/")
+
+
+
+
+
+
+if __name__=="__main__":
+
+    app.run(debug=True)
--- database.py
+++ database.py
@@ -2,65 +2,108 @@ import sqlite3
 
 
 def get_db():
+
     conn = sqlite3.connect("soccer.db")
+
     conn.row_factory = sqlite3.Row
+
     return conn
+
 
 
 def create_tables():
 
     conn = get_db()
 
+
     conn.execute("""
-    CREATE TABLE IF NOT EXISTS teams (
+    CREATE TABLE IF NOT EXISTS teams(
+
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+
         name TEXT NOT NULL
+
     )
     """)
 
 
+
     conn.execute("""
-    CREATE TABLE IF NOT EXISTS players (
+    CREATE TABLE IF NOT EXISTS players(
+
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+
         team_id INTEGER,
+
         name TEXT NOT NULL,
-        goals INTEGER,
-        assists INTEGER,
-        FOREIGN KEY(team_id) REFERENCES teams(id)
+
+        FOREIGN KEY(team_id)
+        REFERENCES teams(id)
+
     )
     """)
 
+
+
     conn.execute("""
-    CREATE TABLE IF NOT EXISTS matches (
+    CREATE TABLE IF NOT EXISTS matches(
+
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+
         team_id INTEGER,
+
         opponent TEXT NOT NULL,
+
         date TEXT,
-        location TEXT,
+
         season TEXT,
+
+        location TEXT,
+
         team_score INTEGER,
+
         opponent_score INTEGER,
-        FOREIGN KEY(team_id) REFERENCES teams(id)
+
+
+        FOREIGN KEY(team_id)
+        REFERENCES teams(id)
 
     )
     """)
 
+
+
     conn.execute("""
-        CREATE TABLE IF NOT EXISTS player_stats (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            player_id INTEGER,
-            match_id INTEGER,
-            goals INTEGER DEFAULT 0,
-            assists INTEGER DEFAULT 0,
-            minutes INTEGER DEFAULT 0,
-            FOREIGN KEY(player_id) REFERENCES players(id),
-            FOREIGN KEY(match_id) REFERENCES matches(id)
-        )
-        """)
+    CREATE TABLE IF NOT EXISTS player_stats(
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        player_id INTEGER,
+
+        match_id INTEGER,
+
+        goals INTEGER DEFAULT 0,
+
+        assists INTEGER DEFAULT 0,
+
+        minutes INTEGER DEFAULT 0,
+
+
+        FOREIGN KEY(player_id)
+        REFERENCES players(id),
+
+
+        FOREIGN KEY(match_id)
+        REFERENCES matches(id)
+
+    )
+    """)
 
 
     conn.commit()
+
     conn.close()
+
 
 
 create_tables()
